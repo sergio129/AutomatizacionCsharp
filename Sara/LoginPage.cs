2458664c140653d8abc230fac81c5f2e248489e4
sergio129 <sanayaromero62@gmail.com>
2024-02-07 18:18:35 -0500

    TEst

---
+++ Sara/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sara
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement CampoUsuario => driver.FindElement(By.Id("loginUserName"));
+        public IWebElement CampoContras => driver.FindElement(By.Id("loginPassword"));
+        public IWebElement CampoEstrellas => driver.FindElement(By.XPath("//p[@class='clasification']//label[@for='idRadioSurvey5']"));
+        public IWebElement BotonLogin => driver.FindElement(By.Id("loginIngresar"));
+
+        public void Login(string username, string password)
+        {
+            CampoUsuario.SendKeys(username);
+            CampoContras.SendKeys(password);
+            CampoEstrellas.Click();
+            BotonLogin.Click();
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Sara/TestLogin.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Sara
+{
+    public class TestLogin {
+        IWebDriver driver;
+        LoginPage loginPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://saralabs.grupokonecta.co:8591/externa_frontend/login");
+            loginPage = new LoginPage(driver);
+        }
+
+
+        [Test]
+        public void TestSuccessfulLogin()
+        {
+            loginPage.Login("Sergio.anaya", "Sistemas_890337*");
+            
+
+            // Realiza afirmaciones según el comportamiento después del inicio de sesión exitoso
+          //  Assert.Equals(driver.Title.Contains("Página principal")); // Por ejemplo, verifica el título de la página después del inicio de sesión
+        }
+
+        [Test]
+        public void TestFailedLogin()
+        {
+            loginPage.Login("usuario_invalido", "contrasena_invalida");
+
+            // Realiza afirmaciones según el comportamiento después del inicio de sesión fallido
+           // Assert.IsTrue(driver.PageSource.Contains("Credenciales incorrectas")); // Por ejemplo, verifica un mensaje de error en la página
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+    }
+}
